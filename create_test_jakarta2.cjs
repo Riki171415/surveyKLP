009ad8426c731caea82927cc874efffdd5323f34
Riki171415 <riki171415@users.noreply.github.com>
2026-06-25 21:26:09 +0700

    Cleanup debug scripts

--- create_test_jakarta2.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const d = fs.readFileSync('generate_final_mapping2.cjs', 'utf8');
-
-const getRegionCode = d.split('const puskesmasFile =')[0];
-
-const runCode = getRegionCode + `
-console.log(getRegion("GAMBIR Jl. Tanah Abang I No. 10 Gambir Kota Adm. Jakarta Pusat DKI Jakarta Non Rawat Inap Perkotaan"));
-`;
-fs.writeFileSync('test_jakarta2.cjs', runCode);
--- create_test_jakarta3.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const d = fs.readFileSync('generate_final_mapping2.cjs', 'utf8');
-
-const getRegionCode = d.split('const puskesmasFile =')[0];
-
-const runCode = getRegionCode + `
-console.log(getRegion("TEBET Jl. Tebet Timur Ii No 9 Rt 006 Rw 005 Tebet Kota Adm. Jakarta Selatan DKI Jakarta Non Rawat Inap Perkotaan"));
-`;
-fs.writeFileSync('test_jakarta3.cjs', runCode);
