207f5f538580e6a5aa0d478e9cdeb7cb6b965a24
Riki171415 <riki171415@users.noreply.github.com>
2026-06-25 21:34:35 +0700

    Cleanup debug scripts

--- create_test_bandung.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const d = fs.readFileSync('generate_final_mapping2.cjs', 'utf8');
-
-const getRegionCode = d.split('const puskesmasFile =')[0];
-
-const runCode = getRegionCode + `
-console.log(getRegion("SUKARASA Jl. Geger Kalong Hilir No. 157 Rt 01/Rt 06 Sukasari Kota Bandung Jawa Barat Non Rawat Inap Perkotaan"));
-`;
-fs.writeFileSync('test_bandung.cjs', runCode);
